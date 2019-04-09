b9024952bae09454334828f75e2bd98d5695eb2a
Scott Miller <scott@millergeek.xyz>
2019-04-09 11:12:04 -0400

    add gitset

---
+++ gitset.fish
@@ -0,0 +1,7 @@
+function "gitset"
+    set -q argv[1]
+    and set -l remote $argv[1]
+    or set -l remote origin
+    # git branch -u $remote/(git symbolic-ref --short HEAD)
+    echo $remote/(git symbolic-ref --short HEAD)
+end
